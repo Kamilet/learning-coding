5d7ae504c7724d598b85b22546c6d62e7c4d3341
Kamilet <hi@kamilet.cn>
2018-01-30 16:45:45 +0800

    unsolved

---
+++ simple-program/check-io-solutions/determinant.py
@@ -0,0 +1,58 @@
+'''
+求矩阵的行列式
+'''
+
+
+
+def checkio(matrix):
+    if len(matrix) == 1:
+        return matrix[0][0]
+    else:
+        return sm_det(matrix)
+
+
+checkio([[4,3], [6,3]]) == -6
+checkio([[1, 3, 2],
+         [1, 1, 4],
+         [2, 2, 1]]) == 14
+a = checkio([[2,7,6,4,2,0],[3,0,8,2,5,6],[3,8,9,1,0,3],[9,4,5,0,8,6],[8,9,0,6,4,6],[1,6,3,0,7,1]])
+print(a) # -140558
+
+'''
+# 有问题，待修复
+
+# Using sMartix ---------------------------------
+# https://github.com/Kamilet/Simple-Matrix-python
+
+def sm_det(matrix, force=False):
+    _row, _colume = sm_check(matrix)
+    _det = 0
+    if _row == 2:
+        _det = matrix[1][1] * matrix[0][0] - matrix[0][1] * matrix[1][0]
+        return _det
+    for _r in range(_colume):
+        _temp1 = 1
+        _temp2 = 1
+        for _c in range(_row):
+            _temp1 *= matrix[_c][(_r+_c)%_colume]
+            _temp2 *= matrix[_c][(_r-_c)%_colume]
+        _det = _det + _temp1 - _temp2
+    return _det
+
+
+def sm_check(matrix):
+    try:
+        _row = len(matrix)
+        _colume = len(matrix[0])
+        if _colume:
+            for items in matrix:
+                if len(items) != _colume:
+                    return False
+            return _row, _colume
+        else:
+            return False
+    except IndexError:
+        return False
+
+# End ----------------------------
+'''
